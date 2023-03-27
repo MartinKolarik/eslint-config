0ffae2961571d05acd7ded5aa5e48b4d883fbc5e
Martin Kolárik <martin@kolarik.sk>
2023-03-27 12:34:45 +0200

    Add comma-dangle for functions

--- .eslintrc.js
+++ .eslintrc.js
@@ -111,6 +111,7 @@ module.exports = {
 				objects: 'always-multiline',
 				imports: 'always-multiline',
 				exports: 'always-multiline',
+				functions: 'always-multiline',
 			},
 		],
 		'comma-spacing': 'error',
